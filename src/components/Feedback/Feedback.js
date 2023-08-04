1944a42597d034d10149a7b5836c4fd4135cbebc
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-08-04 16:03:49 +0200

    Update Feedback.js

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,57 +1,72 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import FeedbackOptions from "./FeedbackOptions";
 import Section from "./Section";
 import Statistics from "./Stats";
 import Notification from "./Notification";
 
-export default class Feedback extends Component {
-  state = {
+const Feedback = () => {
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  handleOnLeaveFeedback = (option) => {
-    this.setState((prevState) => ({
+  const handleOnLeaveFeedback = (option) => {
+    setFeedback((prevState) => ({
+      ...prevState,
       [option]: prevState[option] + 1,
     }));
   };
 
-  countTotalFeedback = () => {
-    const { bad, good, neutral } = this.state;
+  const countTotalFeedback = () => {
+    const { bad, good, neutral } = feedback;
     return bad + good + neutral;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const totalFeedback = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const totalFeedback = countTotalFeedback();
     if (totalFeedback === 0) {
       return 0;
     }
-    return Math.round((this.state.good / totalFeedback) * 100);
+    return Math.round((feedback.good / totalFeedback) * 100);
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
+  useEffect(() => {
+    const totalFeedback = countTotalFeedback();
+    const positivePercentage = countPositiveFeedbackPercentage();
+    // You can also use these values in other calculations or side effects if needed.
+    // For example, you might want to update a backend with these values.
+  }, [feedback]); // The effect will be triggered whenever the 'feedback' state updates.
 
-    const options = Object.keys(this.state);
+  const { good, neutral, bad } = feedback;
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
 
-    return (
-      <div>
-        <Section title="Please leave Feedback">
-          <FeedbackOptions options={options} onLeaveFeedback={this.handleOnLeaveFeedback} />
+  const options = Object.keys(feedback);
+
+  return (
+    <div>
+      <Section title="Please leave Feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={handleOnLeaveFeedback} />
+      </Section>
+
+      {total === 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
         </Section>
+      )}
+    </div>
+  );
+};
 
-        {total === 0 ? (
-          <Notification message="There is no feedback" />
-        ) : (
-          <Section title="Statistics">
-            <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage} />
-          </Section>
-        )}
-      </div>
-    );
-  }
-}
+export default Feedback;
+
 
